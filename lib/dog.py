aaa9c3fd21c036babaf0f9e24ee2a4dad67a2d9c
Your Name <youremail@example.com>
2023-08-21 12:20:11 +0300

    commit message

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-
 class Dog:
     # Class body goes here
-
     #Instance method definition
-    pass
+    def bark(self):
+     print("Woof!")
+    def sit(self):
+     print("The dog is sitting.")
+    
